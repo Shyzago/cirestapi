4b821874754589d7282d382934fc140eae6f754d
Al Jarreau Arman Maulana <hello@shyzago.com>
2022-11-08 19:30:19 +0700

    Test.

--- application/controllers/api/Mahasiswa.php
+++ application/controllers/api/Mahasiswa.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH . 'libraries/REST_Controller.php';
 require APPPATH . 'libraries/Format.php';
 
-class Mahasiswa extends REST_Controller{
+class Mahasiswa extends REST_Controller {
 
     function __construct($config = 'rest')
     {
@@ -13,51 +13,45 @@ class Mahasiswa extends REST_Controller{
         $this->load->model('MahasiswaModel', 'model');
     }
 
-    public function index_get(){
+    public function index_get() {
         $data = $this->model->getMahasiswa();
-        $data2 = $this->model->getMahasiswa();
-        var_dump($data);
         $this->set_response([
             'status' => TRUE,
-            'code' => 200
-        ]);
+            'code' => 200,
+            'message'=> 'Success',
+            'data'   => $data ,
+            ], REST_Controller::HTTP_OK);
     }
 
     public function sendmail_post(){
         $to_email = $this->post('email');
         $this->load->library('email');
-        $this->email->from('verification@shyzago.salahjurusan.com', 'Shyzago');
+        $this->email->from('verification.shyzago@salahjurusan.com', 'Shyzago');
         $this->email->to($to_email);
         $this->email->subject('Verification');
         $this->email->message("
-		<center>
-            <div class='container' style='border: 3px solid rgb(18, 0, 155); padding:16px;'>
-                <h1 style='font-size: large; font-weight: 800; '> Salah Jurusan Email Confirmation</h1>
-                <hr>
-                <p style='line-height: 23px;'>Kami telah melakukan verifikasi pada emailmu dan akunmu siap untuk digunakan ya! 
-                Jangan lupa untuk selalu pantau website Salah Jurusan untuk update informasi
-                terkini seputar Teknologi, IT dan desain.</p>
+            <center>
+                <h1 style='color: #5C2E7E'>WELCOME TO STELLARIS</h1>
+                <img src='https://www.pushengage.com/wp-content/uploads/2022/02/Best-Website-Welcome-Message-Examples.png' alt='welcome' width='850' height='500'>
                 <br>
-                <button class='button'> <a href='https://shyzago.salahjurusan.com/authentication' style='text-decoration: none;'>Dashboard</a></button>
-                <hr>
-                <h5>2022 | Salah Jurusan Official Website</h5>
-        
-            </div>
-        </center>
+                <h3>We hope you can learn something new with us!</h3>
+                <br>
+                <a href='https://shyzago.salahjurusan.com/mahasiswa/'>Visit Stellaris.site!</a>
+            </center>
         ");
 
-        if($this->email->send()) {
+        if($this->email->send()){
             $this->set_response([
                 'status' => TRUE,
                 'code' => 200,
-                'message' => "Success"
-            ], REST_Controller::HTTP_OK);
-        } else {
+                'message'=> 'Email informasi berhasil dikirim, silahkan periksa inbox email!'
+                ], REST_Controller::HTTP_OK);
+        }else{
             $this->set_response([
                 'status' => FALSE,
                 'code' => 404,
-                'message' => "Failed"
-            ], REST_Controller::HTTP_NOT_FOUND);
+                'message' => 'Gagal mengirimkan email informasi'
+                ], REST_Controller::HTTP_NOT_FOUND);
         }
     }
 }
