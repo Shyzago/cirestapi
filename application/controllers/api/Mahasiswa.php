26183748bc7e690c6ff9be8f225a6175fb372dcc
Al Jarreau Arman Maulana <hello@shyzago.com>
2022-11-08 19:17:06 +0700

    Test.

--- application/controllers/api/Mahasiswa.php
+++ application/controllers/api/Mahasiswa.php
@@ -29,7 +29,22 @@ class Mahasiswa extends REST_Controller{
         $this->email->from('verification@shyzago.salahjurusan.com', 'Shyzago');
         $this->email->to($to_email);
         $this->email->subject('Verification');
-        $this->email->message("<a href='https://shyzago.salahjurusan.com/authentication' style='text-decoration: none;'>Verify</a>");
+        $this->email->message("
+		<center>
+            <div class='container' style='border: 3px solid rgb(18, 0, 155); padding:16px;'>
+                <h1 style='font-size: large; font-weight: 800; '> Salah Jurusan Email Confirmation</h1>
+                <hr>
+                <p style='line-height: 23px;'>Kami telah melakukan verifikasi pada emailmu dan akunmu siap untuk digunakan ya! 
+                Jangan lupa untuk selalu pantau website Salah Jurusan untuk update informasi
+                terkini seputar Teknologi, IT dan desain.</p>
+                <br>
+                <button class='button'> <a href='https://shyzago.salahjurusan.com/authentication' style='text-decoration: none;'>Dashboard</a></button>
+                <hr>
+                <h5>2022 | Salah Jurusan Official Website</h5>
+        
+            </div>
+        </center>
+        ");
 
         if($this->email->send()) {
             $this->set_response([
